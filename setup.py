b21a16d1756c21f83f154fde754932587f62d61a
Konrad Weihmann <kweihmann@outlook.com>
2023-05-28 17:23:12 +0200

    Release 1.3.0
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='licensecheck-helper',
-    version='1.2.3',
+    version='1.3.0',
     author='Konrad Weihmann',
     author_email='kweihmann@outlook.com',
     description='Licensing check for different backends',

07ac647a4a2ed582ff6c279d9eadd82a77c1612c
priv-kweihmann <priv-kweihmann@localhost>
2023-12-02 06:15:48 +0000

    Release 1.4.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='licensecheck-helper',
-    version='1.4.1',
+    version='1.4.2',
     author='Konrad Weihmann',
     author_email='kweihmann@outlook.com',
     description='Licensing check for different backends',
